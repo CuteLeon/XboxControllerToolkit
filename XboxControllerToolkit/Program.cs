a8ef906fdb4bccf23f103f477661d5e7c06f302a
Leon <Leon.Engineer@hotmail.com>
2024-08-18 12:36:57 +0800

    tiny;

--- XboxControllerToolkit/Program.cs
+++ XboxControllerToolkit/Program.cs
@@ -27,37 +27,37 @@ namespace XboxControllerToolkit
 
                 var battery = controller.GetBatteryInformation(BatteryDeviceType.Gamepad);
                 Console.WriteLine($"""
-                Battery Information:
-                    Type: {battery.BatteryType}
-                    Level: {battery.BatteryLevel}
-                """);
+                    Battery Information:
+                        Type: {battery.BatteryType}
+                        Level: {battery.BatteryLevel}
+                    """);
                 if (controller.GetCapabilities(DeviceQueryType.Any, out var capabilities))
                 {
                     Console.WriteLine($"""
-                    Capabilities:
-                        Type: {capabilities.Type}
-                        Gamepad:
-                            Buttons: {capabilities.Gamepad.Buttons.ToString().Replace(" ", string.Empty)}
-                            LeftThumbX: {capabilities.Gamepad.LeftThumbX}
-                            LeftThumbY: {capabilities.Gamepad.LeftThumbY}
-                            LeftTrigger: {capabilities.Gamepad.LeftTrigger}
-                            RightThumbX: {capabilities.Gamepad.RightThumbX}
-                            RightThumbY: {capabilities.Gamepad.RightThumbY}
-                            RightTrigger: {capabilities.Gamepad.RightTrigger}
-                        Vibration:
-                            LeftMotorSpeed: {capabilities.Vibration.LeftMotorSpeed}
-                            RightMotorSpeed: {capabilities.Vibration.RightMotorSpeed}
-                        SubType: {capabilities.SubType}
-                        Flags: {capabilities.Flags}
-                    """);
+                        Capabilities:
+                            Type: {capabilities.Type}
+                            Gamepad:
+                                Buttons: {capabilities.Gamepad.Buttons.ToString().Replace(" ", string.Empty)}
+                                LeftThumbX: {capabilities.Gamepad.LeftThumbX}
+                                LeftThumbY: {capabilities.Gamepad.LeftThumbY}
+                                LeftTrigger: {capabilities.Gamepad.LeftTrigger}
+                                RightThumbX: {capabilities.Gamepad.RightThumbX}
+                                RightThumbY: {capabilities.Gamepad.RightThumbY}
+                                RightTrigger: {capabilities.Gamepad.RightTrigger}
+                            Vibration:
+                                LeftMotorSpeed: {capabilities.Vibration.LeftMotorSpeed}
+                                RightMotorSpeed: {capabilities.Vibration.RightMotorSpeed}
+                            SubType: {capabilities.SubType}
+                            Flags: {capabilities.Flags}
+                        """);
                 }
                 var result = controller.GetKeystroke(DeviceQueryType.Any, out var keystroke);
                 Console.WriteLine($"Get Keystroke: [{result.Code:X}] {result.Success}/{result.Failure}");
                 var resultDescriptor = ResultDescriptor.Find(result);
                 Console.WriteLine($"""
-                Keystroke Result Description:
-                    [{resultDescriptor.Code:X}] [{resultDescriptor.Module}] {resultDescriptor.NativeApiCode}/{resultDescriptor.ApiCode}: {resultDescriptor.Description}
-                """);
+                    Keystroke Result Description:
+                        [{resultDescriptor.Code:X}] [{resultDescriptor.Module}] {resultDescriptor.NativeApiCode}/{resultDescriptor.ApiCode}: {resultDescriptor.Description}
+                    """);
 
                 var (stateLeft, stateTop) = Console.GetCursorPosition();
                 while (true)
@@ -67,15 +67,15 @@ namespace XboxControllerToolkit
                         var state = controller.GetState();
                         Console.SetCursorPosition(0, stateTop - 1);
                         Console.Write($"""
-                    Real-time State: [{DateTime.Now:HH:mm:ss.fff}] [SN={state.PacketNumber,10}]
-                        {"Buttons",12} = {state.Gamepad.Buttons.ToString().Replace(" ", string.Empty),-80}
-                        {"LeftThumbX",12} = {state.Gamepad.LeftThumbX,-10}
-                        {"LeftThumbY",12} = {state.Gamepad.LeftThumbY,-10}
-                        {"RightThumbX",12} = {state.Gamepad.RightThumbX,-10}
-                        {"RightThumbY",12} = {state.Gamepad.RightThumbY,-10}
-                        {"LeftTrigger",12} = {state.Gamepad.LeftTrigger,-5}
-                        {"RightTrigger",12} = {state.Gamepad.RightTrigger,-5}
-                    """);
+                            Real-time State: [{DateTime.Now:HH:mm:ss.fff}] [SN={state.PacketNumber,10}]
+                                {"Buttons",12} = {state.Gamepad.Buttons.ToString().Replace(" ", string.Empty),-80}
+                                {"LeftThumbX",12} = {state.Gamepad.LeftThumbX,-10}
+                                {"LeftThumbY",12} = {state.Gamepad.LeftThumbY,-10}
+                                {"RightThumbX",12} = {state.Gamepad.RightThumbX,-10}
+                                {"RightThumbY",12} = {state.Gamepad.RightThumbY,-10}
+                                {"LeftTrigger",12} = {state.Gamepad.LeftTrigger,-5}
+                                {"RightTrigger",12} = {state.Gamepad.RightTrigger,-5}
+                            """);
                         Thread.Sleep(1);
                     }
                     catch (Exception ex)
